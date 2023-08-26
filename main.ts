ab27f6df9c116561189ba8cf4f4b9cb19e20ca8b
Liam Snow <liamsnow03@gmail.com>
2023-08-26 12:39:08 -0400

    Implementing Mathquill

--- main.ts
+++ main.ts
@@ -1,134 +1,122 @@
+declare var MQ: any;
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ObsidianMathquillSettings {
+    inlineIdentifier: string;
+    blockIdentifier: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ObsidianMathquillSettings = {
+    inlineIdentifier: '$',
+    blockIdentifier: '$$'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ObsidanMathquillPlugin extends Plugin {
+    settings: ObsidianMathquillSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        //inject css
+        const cssPath = this.app.plugin['obsidian-mathquill'].directory + 'node_modules/mathquill/build/mathquill.css';
+        if (!document.getElementById('mathquill-styles')) {
+            const link = document.createElement('link');
+            link.id = 'mathquill-styles';
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = cssPath;
+            document.head.appendChild(link);
+        }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        console.log(13, "LOAD");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        const script = document.createElement('script');
+        script.src = 'node_modules/build/mathquill.js';
+        script.onload = () => {
+            console.log("haha");
+            // The script has finished loading and is now available.
+            // You can now safely use the `MQ` variable from the MathQuill library.
+        };
+        document.body.appendChild(script);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        // this.registerMarkdownPostProcessor((el, ctx) => {
+        //     // Your custom processing code here.
+        //     let inlineMathElems = el.querySelectorAll('span.math.inline');
+        //     let blockMathElems = el.querySelectorAll('div.math.display');
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        //     // For inline Math
+        //     inlineMathElems.forEach(elem => {
+        //         console.log(13, "FOUND INLINE");
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        //         let latex = elem.textContent; // get the LaTeX code
+        //         let mathFieldSpan = document.createElement('span');
+        //         let mathField = MQ.MathField(mathFieldSpan, {
+        //             spaceBehavesLikeTab: true,
+        //             handlers: {
+        //                 edit: function () {
+        //                     // handle edits if needed
+        //                 }
+        //             }
+        //         });
+        //         if (latex) {
+        //             mathField.latex(latex);
+        //         }
+        //         else mathField.latex();
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        //         elem.replaceWith(mathFieldSpan); // replace the original LaTeX span with MathQuill
+        //     });
+        // });
 
-	onunload() {
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new ObsidianMathquillSettingTab(this.app, this));
+    }
 
-	}
+    onunload() {
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ObsidianMathquillSettingTab extends PluginSettingTab {
+    plugin: ObsidanMathquillPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: ObsidanMathquillPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        new Setting(containerEl)
+            .setName('Inline Idenifier')
+            .setDesc('TODO')
+            .addText(text => text
+                .setPlaceholder('$')
+                .setValue(this.plugin.settings.inlineIdentifier)
+                .onChange(async (value) => {
+                    this.plugin.settings.inlineIdentifier = value;
+                    await this.plugin.saveSettings();
+                }));
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Block Idenifier')
+            .setDesc('TODO')
+            .addText(text => text
+                .setPlaceholder('$#')
+                .setValue(this.plugin.settings.blockIdentifier)
+                .onChange(async (value) => {
+                    this.plugin.settings.blockIdentifier = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
